5d151eb8a34d2a81096702b4933b1836a187f2fc
zerock <cookieKang@gmail.com>
2019-12-28 01:20:10 +0900

    query dsl

--- src/main/java/org/zerock/board/repo/tboard/TBoardRepositoryCustomImpl.java
+++ src/main/java/org/zerock/board/repo/tboard/TBoardRepositoryCustomImpl.java
@@ -1,41 +1,95 @@
 package org.zerock.board.repo.tboard;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.orm.jpa.persistenceunit.DefaultPersistenceUnitManager;
+import org.zerock.board.domain.QTBoard;
+import org.zerock.board.domain.QTReply;
 import org.zerock.board.domain.TBoard;
 import org.zerock.board.dto.SearchDTO;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
-public class TBoardRepositoryCustomImpl implements TBoardRepositoryCustom {
+public class TBoardRepositoryCustomImpl extends QuerydslRepositorySupport implements TBoardRepositoryCustom {
 
     @PersistenceContext
     private EntityManager entityManager;
 
+    public TBoardRepositoryCustomImpl() {
+        super(TBoard.class);
+    }
+
     @Override
     public List<Object[]> getCustomList(SearchDTO dto, Pageable pageable) {
 
-        log.info("getCustomList................");
+        log.info("custom impl class run................");
 
-        StringBuilder query =  new StringBuilder("select b.bno, b.title, count(r.rno) from TBoard b LEFT JOIN b.replyList r GROUP BY b ");
+        QTBoard board = QTBoard.tBoard;
+        QTReply reply = QTReply.tReply;
 
-        query.append("Order by ");
+        JPQLQuery<TBoard> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(board.replyList, reply);
 
-        Sort.Order order = pageable.getSort().getOrderFor("bno");
+        JPQLQuery<Tuple> tuple =
+                jpqlQuery.select(board.bno, board.title, reply.count() ).groupBy(board.bno);
 
-        query.append(order.getProperty() + " " + order.getDirection() );
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+        tuple.orderBy(board.bno.desc());
 
-        Query query1 = entityManager.createQuery(query.toString());
+        log.info("" + tuple);
 
-        query1.setFirstResult(pageable.getPageNumber());
-        query1.setMaxResults(pageable.getPageSize());
+        List<Tuple> list = tuple.fetch();
 
-        return query1.getResultList();
+//        for (Tuple tuple1 : list) {
+//            log.info("------------------------");
+//            log.info("" + tuple1);
+//        }
+
+        long total = tuple.fetchCount();
+
+        List<Object[]> arrList = list.stream().map(t -> t.toArray()).collect(Collectors.toList());
+
+        return arrList;
     }
+
+
+
+    
+//
+//    @PersistenceContext
+//    private EntityManager entityManager;
+//
+//    @Override
+//    public List<Object[]> getCustomList(SearchDTO dto, Pageable pageable) {
+//
+//        log.info("getCustomList................");
+//
+//        StringBuilder query =  new StringBuilder("select b.bno, b.title, count(r.rno) from TBoard b LEFT JOIN b.replyList r GROUP BY b ");
+//
+//        query.append("Order by ");
+//
+//        Sort.Order order = pageable.getSort().getOrderFor("bno");
+//
+//        query.append(order.getProperty() + " " + order.getDirection() );
+//
+//        Query query1 = entityManager.createQuery(query.toString());
+//
+//        query1.setFirstResult(pageable.getPageNumber());
+//        query1.setMaxResults(pageable.getPageSize());
+//
+//        return query1.getResultList();
+//    }
 }
